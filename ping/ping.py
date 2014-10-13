1e8d451f410e1a967307d2dd8b3d664bd4e87edb
David Shepherd <davidshepherd7@gmail.com>
2014-10-13 17:29:02 +0100

    Convert ping to exercise

--- ping/ping.py
+++ ping/ping.py
@@ -65,42 +65,7 @@ def run_client(server_address, server_port):
     """Ping a UDP pinger server running at the given address
     """
 
-    with Socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
-
-        # The socket stays connected even after this script ends. So in order
-        # to allow the immediate reuse of the socket (so that we can kill and
-        # re-run the server while debugging) we set the following option. This
-        # is potentially dangerous in real code: in rare cases you may get junk
-        # data arriving at the socket.
-        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        # We don't care what port the client uses, leave it as the default
-
-        # Time out after 1 second
-        client_socket.settimeout(1.0)
-
-        # Send 10 empty UDP packets to the server
-        print("Pinging", str(server_address), "on port", server_port)
-        for i in range(10):
-
-            # Send packet to server
-            client_socket.sendto("".encode(), (server_address, server_port))
-
-            # Receive packet and print results
-            try:
-                timer_start = time.time()
-                _, _ = client_socket.recvfrom(1024)
-                timer_stop = time.time()
-
-                # Note: for timing of cpu-bound functions the timeit module
-                # should be used to avoid issues with garbage collection. Since
-                # we are measuring network times this should be fine.
-
-            except socket.timeout:
-                print("Packet lost")
-
-            else:
-                print("Round trip time: {:.3f}ms".format((timer_stop - timer_start)*1000))
+    # Fill in the client side code here.
             
 
     return 0
